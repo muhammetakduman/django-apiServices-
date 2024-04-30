e11ebc3b09fa201268c1798a62f73ce49e7bedc3
muhammet <akdumanmuhammet34@gmail.com>
2024-04-30 23:14:21 +0300

    google verification will be added

---
+++ user/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render,HttpResponse,redirect
+from .forms import RegisterForm ,LoginForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login,authenticate,logout
+from django.contrib import messages
+# Create your views here.
+#Alternatif 1
+def register(request):
+    #Alternatif burda direkt post veya get kontrolü yapmadan None tanımladık.cleancode
+    form = RegisterForm(request.POST) or None
+
+    if form.is_valid():
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+
+        newUser = User(username =username)
+        newUser.set_password(password)
+        newUser.save()
+        login(request,newUser)
+        messages.success(request,("success register."))
+        return redirect("index")
+    context = {
+            "form" : form
+        }
+    return render(request,"register.html",context)
+def loginUser(request):
+    form = LoginForm(request.POST or None)
+    context = {
+        "form":form
+    }
+    if form.is_valid():
+        #enter user and password data
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+        user = authenticate(username = username,password=password)
+        #db in control
+        if user is None:
+            messages.info(request,"Kullanıcı Adı veya parola hatalı")
+            return render(request,"login.html",context)
+        messages.success(request,"Başarıyla Giriş Yaptınız.")
+        login(request,user)
+        return redirect("index")
+    return render(request,"login.html",context)
+def logoutUser(request):
+    logout(request)
+    messages.success(request,"Başariyla çikiş yaptiniz.")
+    return redirect("index")
